ae17127a707a717665c45d1100cc6d0b224fb38d
AO™ <anthony@osawere.com>
2024-11-30 19:39:06 +0100

    Create date.blade.php

---
+++ zero/form/field/date.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'id' => 'date',
+    'name' => 'date',
+    'title' => true,
+    'value' => false,
+    'disable' => false,
+    'obind' => true,
+    'ochange' => false,
+    'validateJS' => true,
+    'btnID' => 'sendBtn',
+    'css' => '',
+
+    'placeholder' => null,
+    'ocomplete' => true,
+])
+
+<x-yale.zero.form.field.text :id="$id" :name="$name" :title="$title" :placeholder="$placeholder" :disable=$disable :obind=$obind :ochange=$ochange :validateJS=$validateJS :btnID=$btnID :value=$value :ocomplete=$ocomplete type="date" {{ $attributes }} />
