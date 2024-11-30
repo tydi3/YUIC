cf7bd55d482362a32bb8bc77de8d963fee60710f
AO™ <anthony@osawere.com>
2024-11-30 19:39:12 +0100

    Create number.blade.php

---
+++ zero/form/field/number.blade.php
@@ -0,0 +1,20 @@
+@props([
+    'id' => null,
+    'name' => null,
+    'title' => true,
+    'value' => '',
+    'disable' => false,
+    'obind' => true,
+    'ochange' => false,
+    'validateJS' => true,
+    'btnID' => 'sendBtn',
+    'css' => '',
+
+    'placeholder' => null,
+    'ocomplete' => true,
+    'step' => '1',
+    'min' => '0',
+    'max' => '',
+])
+
+<x-yale.zero.form.field.text :id="$id" :name="$name" :title="$title" :placeholder="$placeholder" :disable=$disable :obind=$obind :ochange=$ochange :validateJS=$validateJS :btnID=$btnID :value=$value :ocomplete=$ocomplete type="number" step="{{ $step }}" min="{{ $min }}" max="{{ $max }}" {{ $attributes }} />
