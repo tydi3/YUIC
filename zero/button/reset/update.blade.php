af822d3f8cb0d09002a2bb2beb5897ac7c77afb5
AO™ <anthony@osawere.com>
2024-12-08 14:25:01 +0100

    Update update.blade.php

--- zero/button/reset/update.blade.php
+++ zero/button/reset/update.blade.php
@@ -1,6 +1,6 @@
-@props(['label' => 'clear', 'submit' => 'save', 'rowID' => null, 'module' => ''])
+@props(['label' => 'restore', 'submit' => 'save', 'rowID' => null, 'module' => ''])
 @php
 	FormX::set()->button($label, $rowID, $module);
 @endphp
 
-<button wire:loading.attr="disabled" wire:target="{{ $submit }}" @click="$wire.clear('update', '{{ $rowID }}')" onclick="jsHideError('actionBtn');" type="button" class="btn btn-block btn-outline-gray-400 mx-2">{{ $label }}</button>
+<button wire:loading.attr="disabled" wire:target="{{ $submit }}" @click="$wire.restore('update', '{{ $rowID }}')" onclick="jsHideError('actionBtn');" type="button" class="btn btn-block btn-outline-gray-400 mx-2">{{ $label }}</button>
