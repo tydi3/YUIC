dd1ada3aeab2bd03cf435c54d3abcea65b1a8984
AO™ <anthony@osawere.com>
2024-12-08 14:25:10 +0100

    Update notify.blade.php

--- zero/notify.blade.php
+++ zero/notify.blade.php
@@ -1,7 +1,7 @@
 @props(['theme' => 'bootstrap'])
 
 @if (Session::has('successX'))
-	<x-yale.wire.notify.success message="{{ Session::get('successX') }}" :theme=$theme />
+	<x-yale.wire.notify.success :message="Session::get('successX')" :theme=$theme />
 @elseif ($errors->any())
 	<x-yale.wire.notify.error :errors=$errors :theme=$theme />
 @endif
